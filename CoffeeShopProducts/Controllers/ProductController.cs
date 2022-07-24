57240961541a58eb94aaafb6f6b2a96531217a1a
JaraMohnson <jaramohnson@gmail.com>
2022-07-24 19:12:14 -0400

    hi

---
+++ CoffeeShopProducts/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CoffeeShopProducts.Models;
+
+
+namespace CoffeeShopProducts.Controllers
+{
+    public class ProductController : Controller
+    {
+        private CoffeeShopDBContext context = new CoffeeShopDBContext();
+        
+        public IActionResult pIndex()
+        {
+            List<Product> result = context.Products.GroupBy(p => p.Category).Select(p => p.First()).ToList();
+            //List<Product> result = context.Products.DistinctBy(p => p.Category).ToList();
+            return View(result);
+        }
+        public IActionResult Detail(int itemId)
+        {
+            Product p = context.Products.FirstOrDefault(p => p.Id == itemId);
+            return View(p);
+        }
+
+        public IActionResult Category(string category)
+        {
+            List<Product> result = context.Products.Where(p => p.Category == category).ToList();
+            return View(result);
+        }
+
+    }
+}
---
+++ CoffeeShopProducts/Models/CoffeeShopDBContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CoffeeShopProducts.Models
+{
+    public partial class CoffeeShopDBContext : DbContext
+    {
+        public CoffeeShopDBContext()
+        {
+        }
+
+        public CoffeeShopDBContext(DbContextOptions<CoffeeShopDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+                optionsBuilder.UseSqlServer("Data Source=.\\sqlexpress;Initial Catalog=CoffeeShopDB; Integrated Security=SSPI;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Category).HasMaxLength(60);
+
+                entity.Property(e => e.Description).HasMaxLength(255);
+
+                entity.Property(e => e.Name).HasMaxLength(60);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
